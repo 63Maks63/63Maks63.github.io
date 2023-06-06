227bbbdb352f8a84fa213f736959dad4823d8b49
Maks <maks7772001@gmail.com>
2023-06-06 14:59:24 +0300

    1.0

--- JS/Coins.js
+++ JS/Coins.js
@@ -1,52 +1,52 @@
 export const coins = {
-   'Bitcoin': {
-      'abb': 'BTC',
-      'RUB': 2215838.98,
-      'USDT': 10.25,
-   },
-   'BNB Coin': {
-      'abb': 'BNB',
-      'RUB': '150',
-      'USDT': '25',
-   },
-   'Ethereum': {
-      'abb': 'ETH',
-      'RUB': '500',
-      'USDT': '1500',
-   },
-   'Ethereum Classic': {
-      'abb': 'ETC',
-      'RUB': '250000',
-      'USDT': '210',
-   },
-   'Cardano': {
-      'abb': 'ADA',
-      'RUB': '2215838,98',
-      'USDT': '27 354,73',
-   },
-   'Tron': {
-      'abb': 'TRX',
-      'RUB': '2215838,98',
-      'USDT': '27 354,73',
-   },
-   'Litecoin': {
-      'abb': 'LTC',
-      'RUB': '2215838,98',
-      'USDT': '27 354,73',
-   },
-   'Dash': {
-      'abb': 'DASH',
-      'RUB': '2215838,98',
-      'USDT': '27 354,73',
-   },
-   'Ripple': {
-      'abb': 'XPR',
-      'RUB': '2215838,98',
-      'USDT': '27 354,73',
-   },
-   'Stellar': {
-      'abb': 'XML',
-      'RUB': '2215838,98',
-      'USDT': '27 354,73',
-   },
+  Bitcoin: {
+    abb: "BTC",
+    RUB: 2245435,
+    USDT: 27800,
+  },
+  "BNB Coin": {
+    abb: "BNB",
+    RUB: 25532,
+    USDT: 313,
+  },
+  Ethereum: {
+    abb: "ETH",
+    RUB: 153573,
+    USDT: 1907,
+  },
+  "Ethereum Classic": {
+    abb: "ETC",
+    RUB: 1480,
+    USDT: 18.5,
+  },
+  Cardano: {
+    abb: "ADA",
+    RUB: 30,
+    USDT: 0.38,
+  },
+  Tron: {
+    abb: "TRX",
+    RUB: 5475,
+    USDT: 0.06,
+  },
+  Litecoin: {
+    abb: "LTC",
+    RUB: 7410,
+    USDT: 92,
+  },
+  Dash: {
+    abb: "DASH",
+    RUB: 3500,
+    USDT: 43.5,
+  },
+  Ripple: {
+    abb: "XPR",
+    RUB: 4008,
+    USDT: 0.5,
+  },
+  Stellar: {
+    abb: "XML",
+    RUB: 7.2,
+    USDT: 0.09,
+  },
 };
